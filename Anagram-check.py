0fe9544c21b6c2533c77dd6f58f3be83226a9591
tejaswis1508 <inboxofteju@gmail.com>
2022-12-22 19:45:05 +0530

    Update and rename Anagram-check to Anagram-check.py

--- Anagram-check.py
+++ Anagram-check.py
@@ -2,6 +2,7 @@
 At frist we check the length of both strings; if they are equal we continue and if not we print they are not anagrams. 
 Sort both of the strings after converting them to character arrays. Verify that the sorted arrays are all equal. 
 Print anagrams if they are equal; otherwise, do not."""
+
 import numpy as np 
 
 st1 = "@#$%&"
